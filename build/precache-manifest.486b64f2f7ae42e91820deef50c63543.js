429620fe73911178ee1f05e34abf6137a361f6e6
mbabali iryn <mbabaliiryn@gmail.com>
2020-10-22 16:05:17 +0300

    Your awesome message

---
+++ build/precache-manifest.486b64f2f7ae42e91820deef50c63543.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2e351bb7fc4b0766fba8f9831305946",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "15836c574b71f60dc0ae",
+    "url": "/portfolio/static/css/2.f76e3c0a.chunk.css"
+  },
+  {
+    "revision": "b8c855d097c02189bedd",
+    "url": "/portfolio/static/css/main.f9c05514.chunk.css"
+  },
+  {
+    "revision": "15836c574b71f60dc0ae",
+    "url": "/portfolio/static/js/2.da384d71.chunk.js"
+  },
+  {
+    "revision": "cfaacf686fe839c9a66acfa24db7c28c",
+    "url": "/portfolio/static/js/2.da384d71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8c855d097c02189bedd",
+    "url": "/portfolio/static/js/main.90def822.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
